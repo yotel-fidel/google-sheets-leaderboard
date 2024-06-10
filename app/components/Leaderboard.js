1be69d80d9c0a85ed9a3b40be79da470bd701458
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-10 10:24:32 +0400

    fix: display images, booked dems and mds

--- app/components/Leaderboard.js
+++ app/components/Leaderboard.js
@@ -4,14 +4,16 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 const Leaderboard = () => {
-  const [salesInfo, setSalesInfo] = useState([]);
+  const [bookedDemsData, setBookedDemsData] = useState([]);
+  const [bookedMDSData, setBookedMDSData] = useState([]);
 
   useEffect(() => {
     const fetchSalesInfo = async () => {
       try {
         const response = await fetch('/api/getSheetDataByTotal');
         const data = await response.json();
-        setSalesInfo(data.salesInfo);
+        setBookedDemsData(data.bookedDemsData);
+        setBookedMDSData(data.bookedMDSData);
       } catch (error) {
         console.error("Error fetching sales info:", error);
       }
@@ -21,9 +23,10 @@ const Leaderboard = () => {
   }, []);
 
   return (
-    <div className="leaderboard-container max-w-4xl mx-auto p-4 bg-gray-100 rounded-lg shadow-lg">
-      <h1 className="text-2xl text-black font-bold text-center mb-6">Sales Leaderboard by Total</h1>
-      {salesInfo && salesInfo.map((info, index) => (
+    <div className="leaderboard-container flex flex-wrap justify-between gap-2 mx-auto p-4 bg-gray-100 rounded-lg shadow-lg">
+      <div className='flex-1'>
+      <h1 className="text-2xl text-black font-bold text-center mb-6">Sales Leaderboard (Booked Dems) Total</h1>
+      {bookedDemsData && bookedDemsData.map((info, index) => (
         <Link 
           key={index} 
           href={`/sales-person/${info.name.toLowerCase().replace(/\s/g, "_")}`}
@@ -38,13 +41,29 @@ const Leaderboard = () => {
               </div>
               <div className="person-image-container flex items-center">
                 <div className="image-container w-12 h-12 rounded-full overflow-hidden mr-4">
-                  {/* <img 
-                    src={`http://localhost/test-wp/wp-content/uploads/2024/05/${info.name.toLowerCase().replace(/\s/g, "_")}.webp`} 
+                  {info.profileImg && <img 
+                    src={`https://drive.google.com/thumbnail?id=${info.profileImg}&sz=w50`} 
                     alt={info.name} 
-                    className="w-full h-full object-cover" 
-                  /> */}
+                    className="w-full h-full object-cover"
+                    referrerPolicy="no-referrer"
+                  />}
+                  {!info.profileImg && <img 
+                    src={'/assets/images/default-person.webp'}
+                    alt={info.name} 
+                    className="w-full h-full object-cover"
+                    referrerPolicy="no-referrer"
+                  />}
                 </div>
-                <p className="sales-person-name text-gray-700">{info.name}</p>
+                <div>
+                    <p className="sales-person-name text-gray-700">{info.name}</p>
+                    <Link 
+                    key={index} 
+                    href={`/team/${info.team.toLowerCase().replace(/\s/g, "_")}`}
+                    passHref
+                    >
+                        <p className="sales-person-name text-[12px] text-gray-700 hover:underline">{info.team}</p>
+                    </Link>
+                </div>
               </div>
             </div>
             <div className="booking-number text-xl font-semibold text-blue-600">
@@ -53,6 +72,59 @@ const Leaderboard = () => {
           </div>
         </Link>
       ))}
+      </div>
+
+      <div className='flex-1'>
+      <h1 className="text-2xl text-black font-bold text-center mb-6">Sales Leaderboard (Booked MDS) Total</h1>
+      {bookedMDSData && bookedMDSData.map((info, index) => (
+        <Link 
+          key={index} 
+          href={`/sales-person/${info.name.toLowerCase().replace(/\s/g, "_")}`}
+          passHref
+        >
+          <div 
+            className="item-container flex justify-between items-center p-4 bg-white mb-4 rounded-lg shadow-md hover:bg-gray-50 cursor-pointer"
+          >
+            <div className="left-side-container flex items-center">
+              <div className="ranking-container text-lg font-bold text-gray-700 mr-4">
+                Rank {index + 1}
+              </div>
+              <div className="person-image-container flex items-center">
+                <div className="image-container w-12 h-12 rounded-full overflow-hidden mr-4">
+                  {info.profileImg && <img 
+                    src={`https://drive.google.com/thumbnail?id=${info.profileImg}&sz=w50`} 
+                    alt={info.name} 
+                    className="w-full h-full object-cover"
+                    referrerPolicy="no-referrer"
+                  />}
+                  {!info.profileImg && <img 
+                    src={'/assets/images/default-person.webp'}
+                    alt={info.name} 
+                    className="w-full h-full object-cover"
+                    referrerPolicy="no-referrer"
+                  />}
+                </div>
+                <div>
+                    <p className="sales-person-name text-gray-700">{info.name}</p>
+                    <Link 
+                    key={index} 
+                    href={`/team/${info.team.toLowerCase().replace(/\s/g, "_")}`}
+                    passHref
+                    >
+                        <p className="sales-person-name text-[12px] text-gray-700 hover:underline">{info.team}</p>
+                    </Link>
+                </div>
+              </div>
+            </div>
+            <div className="booking-number text-xl font-semibold text-blue-600">
+              {info.total}
+            </div>
+          </div>
+        </Link>
+      ))}
+      </div>
+
+
     </div>
   );
 };
