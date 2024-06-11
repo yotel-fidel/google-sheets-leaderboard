fb42ace06bb866d244a55e455b94d2495da13650
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-11 16:48:11 +0400

    fix(rankingCard): fix position of rank depending on data displayed

--- app/components/RankingCard.js
+++ app/components/RankingCard.js
@@ -25,35 +25,40 @@ export default function RankingCard({ info, index, currentWeekNumber, isCurrency
             className='min-w-[300px]'
             passHref
         >
-            <div className="item-container flex justify-between items-center p-4 bg-white rounded-lg shadow-md hover:bg-gray-50 cursor-pointer">
+            <div className="item-container flex gap-1 justify-between items-center p-4 bg-white rounded-lg shadow-md hover:bg-gray-50 cursor-pointer">
                 <div className="left-side-container flex items-center">
-                    <div className="ranking-container text-lg font-bold text-gray-700 mr-4">
+                    {!isCurrency && (<div className="ranking-container text-lg font-bold text-gray-700 mr-4">
                         Rank {index + 1}
-                    </div>
-                    <div className="person-image-container flex items-center">
-                        <div className="image-container w-12 h-12 rounded-full overflow-hidden mr-4">
-                            {info.profileImg ? (
-                                <img
-                                    src={`https://drive.google.com/thumbnail?id=${info.profileImg}&sz=w50`}
-                                    alt={info.name}
-                                    className="w-full h-full object-cover"
-                                    referrerPolicy="no-referrer"
-                                />
-                            ) : (
-                                <img
-                                    src={'/assets/images/default-person.webp'}
-                                    alt={info.name}
-                                    className="w-full h-full object-cover"
-                                    referrerPolicy="no-referrer"
-                                />
-                            )}
-                        </div>
-                        <div>
-                            <p className="sales-person-name text-gray-700">{info.name}</p>
-                            <p
-                                className="sales-person-name text-[12px] text-gray-700 hover:underline"
-                                onClick={(e) => handlePersonTeamClick(e, info.team)}
-                            >{info.team}</p>
+                    </div>)}
+                    <div className="person-image-container flex flex-col items-left gap-1">
+                        {isCurrency && (<div className="ranking-container text-lg font-bold text-gray-700 mr-4">
+                            Rank {index + 1}
+                        </div>)}
+                        <div className='flex flex-wrap gap-3'>
+                            <div className="image-container w-12 h-12 rounded-full overflow-hidden">
+                                {info.profileImg ? (
+                                    <img
+                                        src={`https://drive.google.com/thumbnail?id=${info.profileImg}&sz=w50`}
+                                        alt={info.name}
+                                        className="w-full h-full object-cover"
+                                        referrerPolicy="no-referrer"
+                                    />
+                                ) : (
+                                    <img
+                                        src={'/assets/images/default-person.webp'}
+                                        alt={info.name}
+                                        className="w-full h-full object-cover"
+                                        referrerPolicy="no-referrer"
+                                    />
+                                )}
+                            </div>
+                            <div>
+                                <p className="sales-person-name text-gray-700">{info.name}</p>
+                                <p
+                                    className="sales-person-name text-[12px] text-gray-700 hover:underline"
+                                    onClick={(e) => handlePersonTeamClick(e, info.team)}
+                                >{info.team}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
