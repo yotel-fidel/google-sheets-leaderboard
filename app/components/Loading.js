8252f78b9e970a80b4a08de84c89b445b46eec65
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-11 11:54:11 +0400

    fix: update height of loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="flex justify-center items-center bg-gray-100">
             <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500"></div>
         </div>
     );
