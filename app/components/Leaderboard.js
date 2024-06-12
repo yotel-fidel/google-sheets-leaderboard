f7b59d42e1585f5b70e2dad9daec98d3115f5dc9
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-12 18:09:23 +0400

    feat(Leaderboard): display data depending on time period

--- app/components/Leaderboard.js
+++ app/components/Leaderboard.js
@@ -7,6 +7,33 @@ import Loading from './Loading';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import RankingCard from './RankingCard';
 
+import { Check, ChevronsUpDown } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+const dataOptionList = [
+  {
+    value: "default",
+    label: "Default",
+  },
+  ...Array.from({ length: 52 }, (v, i) => ({
+    value: `week${i + 1}`,
+    label: `Week ${i + 1}`,
+  })),
+];
 
 const Leaderboard = () => {
   const [bookedDemsData, setBookedDemsData] = useState([]);
@@ -16,7 +43,11 @@ const Leaderboard = () => {
   const [salesSDRData, setSalesSDRData] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
   const { currentWeekNumber, currentYear } = getCurrentWeekAndYear();
+  const [timePeriod, setTimePeriod] = useState(currentWeekNumber);
+
 
   useEffect(() => {
     const fetchSalesInfo = async () => {
@@ -30,9 +61,9 @@ const Leaderboard = () => {
         setSalesSDRData(data.salesSDRData);
         setLoading(false);
 
-        // console.log("Week number: ", currentWeekNumber)
+        // console.log("Week number: ", timePeriod)
         // console.log("Year number: ", currentYear)
-        // console.log("testing date: ", getWeekRange(currentWeekNumber, currentYear))
+        // console.log("testing date: ", getWeekRange(timePeriod, currentYear))
       } catch (error) {
         console.error("Error fetching sales info:", error);
       }
@@ -43,7 +74,51 @@ const Leaderboard = () => {
 
   return (
     <div className="leaderboard-container mx-auto p-4 min-h-screen bg-gray-100 rounded-lg shadow-lg">
-      <h1 className='text-black text-center mb-2'>{getWeekRange(currentWeekNumber, currentYear)}</h1>
+      <h1 className='text-black text-center mb-2'>{timePeriod && getWeekRange(timePeriod, currentYear)}</h1>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {value
+              ? dataOptionList.find((dataOptionItem) => dataOptionItem.value === value)?.label
+              : "Filter Data..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search Options..." />
+            <CommandList>
+              <CommandEmpty>No option found.</CommandEmpty>
+              <CommandGroup className="max-h-[200px] overflow-y-auto">
+                {dataOptionList.map((dataOptionItem) => (
+                  <CommandItem
+                    key={dataOptionItem.value}
+                    value={dataOptionItem.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false)
+                      setTimePeriod(currentValue === 'default' ? currentWeekNumber : currentValue.replace(/week/g, ""))
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === dataOptionItem.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {dataOptionItem.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
       <Tabs defaultValue="bookedDems" className="w-full">
         <TabsList>
           <TabsTrigger value="bookedDems">Booked Dems</TabsTrigger>
@@ -58,8 +133,8 @@ const Leaderboard = () => {
             <Loading />
           </div>)}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-            {bookedDemsData && bookedDemsData.sort((a, b) => b.sales[currentWeekNumber - 1] - a.sales[currentWeekNumber - 1]).map((info, index) => (
-              <RankingCard key={index} info={info} index={index} currentWeekNumber={currentWeekNumber} isShowTeam={true} />
+            {bookedDemsData && bookedDemsData.sort((a, b) => b.sales[timePeriod - 1] - a.sales[timePeriod - 1]).map((info, index) => (
+              <RankingCard key={index} info={info} index={index} currentWeekNumber={timePeriod} isShowTeam={true} />
             ))}
           </div>
         </TabsContent>
@@ -69,8 +144,8 @@ const Leaderboard = () => {
             <Loading />
           </div>)}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-            {bookedMDSData && bookedMDSData.sort((a, b) => b.sales[currentWeekNumber - 1] - a.sales[currentWeekNumber - 1]).map((info, index) => (
-              <RankingCard key={index} info={info} index={index} currentWeekNumber={currentWeekNumber} isShowTeam={true} />
+            {bookedMDSData && bookedMDSData.sort((a, b) => b.sales[timePeriod - 1] - a.sales[timePeriod - 1]).map((info, index) => (
+              <RankingCard key={index} info={info} index={index} currentWeekNumber={timePeriod} isShowTeam={true} />
             ))}
           </div>
         </TabsContent>
@@ -80,8 +155,8 @@ const Leaderboard = () => {
             <Loading />
           </div>)}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-            {satDemsData && satDemsData.sort((a, b) => b.sales[currentWeekNumber - 1] - a.sales[currentWeekNumber - 1]).map((info, index) => (
-              <RankingCard key={index} info={info} index={index} currentWeekNumber={currentWeekNumber} isShowTeam={true} />
+            {satDemsData && satDemsData.sort((a, b) => b.sales[timePeriod - 1] - a.sales[timePeriod - 1]).map((info, index) => (
+              <RankingCard key={index} info={info} index={index} currentWeekNumber={timePeriod} isShowTeam={true} />
             ))}
           </div>
         </TabsContent>
@@ -91,8 +166,8 @@ const Leaderboard = () => {
             <Loading />
           </div>)}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-            {satMDSData && satMDSData.sort((a, b) => b.sales[currentWeekNumber - 1] - a.sales[currentWeekNumber - 1]).map((info, index) => (
-              <RankingCard key={index} info={info} index={index} currentWeekNumber={currentWeekNumber} isShowTeam={true} />
+            {satMDSData && satMDSData.sort((a, b) => b.sales[timePeriod - 1] - a.sales[timePeriod - 1]).map((info, index) => (
+              <RankingCard key={index} info={info} index={index} currentWeekNumber={timePeriod} isShowTeam={true} />
             ))}
           </div>
         </TabsContent>
@@ -102,8 +177,8 @@ const Leaderboard = () => {
             <Loading />
           </div>)}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-            {salesSDRData && salesSDRData.sort((a, b) => b.sales[currentWeekNumber - 1] - a.sales[currentWeekNumber - 1]).map((info, index) => (
-              <RankingCard key={index} info={info} index={index} currentWeekNumber={currentWeekNumber} isShowTeam={true} isCurrency={true} />
+            {salesSDRData && salesSDRData.sort((a, b) => b.sales[timePeriod - 1] - a.sales[timePeriod - 1]).map((info, index) => (
+              <RankingCard key={index} info={info} index={index} currentWeekNumber={timePeriod} isShowTeam={true} isCurrency={true} />
             ))}
           </div>
         </TabsContent>
