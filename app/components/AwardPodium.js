e39aa217526c7f735585aed74293c6d8a5ca1005
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-13 17:31:36 +0400

    feat: awardPodium component

---
+++ app/components/AwardPodium.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import AwardPodiumCard from './AwardPodiumCard';
+
+const AwardPodium = ({ first, second, third, isCurrency, currentWeekNumber, className = '' }) => {
+    const formatLargeCurrency = (value) => {
+        const removedCurrency = value[0];
+        const money = value.substring(1).replace(/,/g, '');
+
+        if (parseFloat(money) >= 1000) {
+            return `${removedCurrency}${(parseFloat(money) / 1000).toFixed(1)}k`;
+        }
+        return `${removedCurrency}${money}`;
+    };
+
+    return (
+        <div className={`relative w-full max-w-[500px] ${className}`}>
+            <img
+                src="/assets/images/award_podium.webp"
+                alt="Podium"
+                className="w-full h-auto"
+            />
+            <div className="absolute top-0 left-1/2 transform translate-x-[-50%] gap-[15%] flex items-end justify-around">
+                <AwardPodiumCard info={second} currentWeekNumber={currentWeekNumber} className='mb-10' />
+                <AwardPodiumCard info={first} currentWeekNumber={currentWeekNumber} className='mb-24' />
+                <AwardPodiumCard info={third} currentWeekNumber={currentWeekNumber} className='mb-6' />
+            </div>
+        </div >
+    );
+};
+
+export default AwardPodium;
