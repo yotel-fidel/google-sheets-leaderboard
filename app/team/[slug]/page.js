6c8a6a12bb45ae34781737c34827653143799b82
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-20 16:23:23 +0400

    refactor: use sortDataBasedOnPeriod func and DATA_OPTION_LIST constant

--- app/team/[slug]/page.js
+++ app/team/[slug]/page.js
@@ -13,7 +13,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { getWeekRange, getCurrentWeekAndYear, parsePeriodString, getDateRange } from '@/app/_utils'
+import { getWeekRange, sortDataBasedOnPeriod, getCurrentWeekAndYear, parsePeriodString, getDateRange } from '@/app/_utils'
 import Loading from '@/app/components/Loading';
 
 
@@ -37,7 +37,7 @@ import {
 } from "@/components/ui/popover"
 import RankingCard from '@/app/components/RankingCard';
 import AwardPodium from '@/app/components/AwardPodium';
-import { PERIOD_LIST } from '@/lib/constants';
+import { PERIOD_LIST, DATA_OPTION_LIST } from '@/lib/constants';
 
 ChartJS.register(
   CategoryScale,
@@ -62,33 +62,6 @@ export const options = {
   },
 };
 
-const dataOptionList = [
-  {
-    value: "default",
-    label: "Default",
-  },
-  ...Array.from({ length: 52 }, (v, i) => ({
-    value: `week${i + 1}`,
-    label: `Week ${i + 1}`,
-  })),
-  ...[
-    { value: "january1", label: "January" },
-    { value: "february2", label: "February" },
-    { value: "march3", label: "March" },
-    { value: "april4", label: "April" },
-    { value: "may5", label: "May" },
-    { value: "june6", label: "June" },
-    { value: "july7", label: "July" },
-    { value: "august8", label: "August" },
-    { value: "september9", label: "September" },
-    { value: "october10", label: "October" },
-    { value: "november11", label: "November" },
-    { value: "december12", label: "December" }
-  ]
-];
-
-
-
 const SalesTeamPage = ({ params }) => {
   const [bookedDemsData, setBookedDemsData] = useState(null);
   const [bookedMDSData, setBookedMDSData] = useState(null);
@@ -101,37 +74,6 @@ const SalesTeamPage = ({ params }) => {
   const [sortedSatDemsData, setSortedSatDemsData] = useState(null);
   const [sortedSatMDSData, setSortedSatMDSData] = useState(null);
   const [sortedSalesSDRData, setSortedSalesSDRData] = useState([]);
-
-  // Helper function to create a deep copy of the array and sort it
-  const sortData = (dataArray, period, periodNumber, isCurrency = false) => {
-    return [...dataArray].sort((a, b) => {
-      let aValue, bValue;
-
-      if (period.toLowerCase().startsWith(PERIOD_LIST.WEEKLY.toLowerCase())) {
-        const weekIndex = periodNumber - 1;
-        aValue = isCurrency
-          ? parseFloat(a.weekly[weekIndex].substring(1).replace(/,/g, ''))
-          : Number(a.weekly[weekIndex]);
-        bValue = isCurrency
-          ? parseFloat(b.weekly[weekIndex].substring(1).replace(/,/g, ''))
-          : Number(b.weekly[weekIndex]);
-      } else {
-        // NO MINUS 1 (like in weekIndex) AS IT IS CALLING AN OBJECT
-        const month = periodNumber;
-        // console.log("MONTH: ", month)
-        // console.log("DATA a & b: ", Number(a.monthly[month]), Number(b.monthly[month]))
-        aValue = isCurrency
-          ? parseFloat(a.monthly[month].substring(1).replace(/,/g, ''))
-          : Number(a.monthly[month]);
-        bValue = isCurrency
-          ? parseFloat(b.monthly[month].substring(1).replace(/,/g, ''))
-          : Number(b.monthly[month]);
-      }
-
-      return bValue - aValue;
-    });
-  };
-
 
   const [loading, setLoading] = useState(true);
 
@@ -151,17 +93,17 @@ const SalesTeamPage = ({ params }) => {
         const response = await fetch(`/api/getSheetDataByTeam?team=${team}`);
         const data = await response.json();
 
-        setBookedDemsData(sortData(data.bookedDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setBookedMDSData(sortData(data.bookedMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSatDemsData(sortData(data.satDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSatMDSData(sortData(data.satMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSalesSDRData(sortData(data.salesSDRData, PERIOD_LIST.WEEKLY, currentWeekNumber, true));
+        setBookedDemsData(sortDataBasedOnPeriod(data.bookedDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setBookedMDSData(sortDataBasedOnPeriod(data.bookedMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSatDemsData(sortDataBasedOnPeriod(data.satDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSatMDSData(sortDataBasedOnPeriod(data.satMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSalesSDRData(sortDataBasedOnPeriod(data.salesSDRData, PERIOD_LIST.WEEKLY, currentWeekNumber, true));
 
-        setSortedBookedDemsData(sortData(data.bookedDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSortedBookedMDSData(sortData(data.bookedMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSortedSatDemsData(sortData(data.satDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSortedSatMDSData(sortData(data.satMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
-        setSortedSalesSDRData(sortData(data.salesSDRData, PERIOD_LIST.WEEKLY, currentWeekNumber, true));
+        setSortedBookedDemsData(sortDataBasedOnPeriod(data.bookedDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSortedBookedMDSData(sortDataBasedOnPeriod(data.bookedMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSortedSatDemsData(sortDataBasedOnPeriod(data.satDemsData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSortedSatMDSData(sortDataBasedOnPeriod(data.satMDSData, PERIOD_LIST.WEEKLY, currentWeekNumber));
+        setSortedSalesSDRData(sortDataBasedOnPeriod(data.salesSDRData, PERIOD_LIST.WEEKLY, currentWeekNumber, true));
 
         setLoading(false);
       } catch (error) {
@@ -194,7 +136,7 @@ const SalesTeamPage = ({ params }) => {
                   className="w-[200px] justify-between"
                 >
                   {value
-                    ? dataOptionList.find((dataOptionItem) => dataOptionItem.value === value)?.label
+                    ? DATA_OPTION_LIST.find((dataOptionItem) => dataOptionItem.value === value)?.label
                     : "Filter Data..."}
                   <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
@@ -205,7 +147,7 @@ const SalesTeamPage = ({ params }) => {
                   <CommandList>
                     <CommandEmpty>No option found.</CommandEmpty>
                     <CommandGroup className="max-h-[200px] overflow-y-auto">
-                      {dataOptionList.map((dataOptionItem) => (
+                      {DATA_OPTION_LIST.map((dataOptionItem) => (
                         <CommandItem
                           key={dataOptionItem.value}
                           value={dataOptionItem.value}
@@ -226,11 +168,11 @@ const SalesTeamPage = ({ params }) => {
                             // console.log("Before setting sortedbookedDems: ", newValue.period)
                             // console.log("Period number: ", newValue.number)
 
-                            setSortedBookedDemsData(sortData(bookedDemsData, newValue.period, periodNumber));
-                            setSortedBookedMDSData(sortData(bookedMDSData, newValue.period, periodNumber));
-                            setSortedSatDemsData(sortData(satDemsData, newValue.period, periodNumber));
-                            setSortedSatMDSData(sortData(satMDSData, newValue.period, periodNumber));
-                            setSortedSalesSDRData(sortData(salesSDRData, newValue.period, periodNumber, true));
+                            setSortedBookedDemsData(sortDataBasedOnPeriod(bookedDemsData, newValue.period, periodNumber));
+                            setSortedBookedMDSData(sortDataBasedOnPeriod(bookedMDSData, newValue.period, periodNumber));
+                            setSortedSatDemsData(sortDataBasedOnPeriod(satDemsData, newValue.period, periodNumber));
+                            setSortedSatMDSData(sortDataBasedOnPeriod(satMDSData, newValue.period, periodNumber));
+                            setSortedSalesSDRData(sortDataBasedOnPeriod(salesSDRData, newValue.period, periodNumber, true));
                           }}
                         >
                           <Check
@@ -349,7 +291,7 @@ const SalesTeamPage = ({ params }) => {
                 {sortedSalesSDRData.map((info, index) => {
                   if (index >= 3) {
                     return (
-                      <RankingCard key={index} info={info} index={index} periodObject={timePeriod} />
+                      <RankingCard key={index} info={info} index={index} periodObject={timePeriod} isCurrency={true} />
                     );
                   }
                   return null;
