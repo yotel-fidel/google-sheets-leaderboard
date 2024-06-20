94f2774d292a64da16b072715ae352741bceca1d
Fidel Lim <fidel.lim@yotelecom.co.uk>
2024-06-20 09:07:12 +0400

    fix: add periodObject as a prop

--- app/components/AwardPodium.js
+++ app/components/AwardPodium.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import AwardPodiumCard from './AwardPodiumCard';
 
-const AwardPodium = ({ first, second, third, isCurrency, currentWeekNumber, className = '' }) => {
-    const formatLargeCurrency = (value) => {
-        const removedCurrency = value[0];
-        const money = value.substring(1).replace(/,/g, '');
-
-        if (parseFloat(money) >= 1000) {
-            return `${removedCurrency}${(parseFloat(money) / 1000).toFixed(1)}k`;
-        }
-        return `${removedCurrency}${money}`;
-    };
-
+const AwardPodium = ({ first, second, third, isCurrency, periodObject, className = '' }) => {
     return (
         <div className={`relative w-full max-w-[500px] ${className}`}>
             <img
@@ -19,10 +9,10 @@ const AwardPodium = ({ first, second, third, isCurrency, currentWeekNumber, clas
                 alt="Podium"
                 className="w-full h-auto"
             />
-            <div className="absolute top-0 left-1/2 transform translate-x-[-50%] gap-[15%] flex items-end justify-around">
-                <AwardPodiumCard info={second} currentWeekNumber={currentWeekNumber} isCurrency={isCurrency} className='-mb-[4vw] sm:mb-10' />
-                <AwardPodiumCard info={first} currentWeekNumber={currentWeekNumber} isCurrency={isCurrency} className='mb-[8vw] sm:mb-24' />
-                <AwardPodiumCard info={third} currentWeekNumber={currentWeekNumber} isCurrency={isCurrency} className='-mb-[7.5vw] sm:mb-6' />
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 flex items-end justify-around gap-[7vw] min-[450px]:gap-[11.5vw] md:gap-[5.25vw] lg:gap-[3vw] 2xl:gap-[2vw]">
+                <AwardPodiumCard info={second} periodObject={periodObject} isCurrency={isCurrency} className='-mb-[4vw] sm:mb-10' />
+                <AwardPodiumCard info={first} periodObject={periodObject} isCurrency={isCurrency} className='mb-[8vw] sm:mb-24' />
+                <AwardPodiumCard info={third} periodObject={periodObject} isCurrency={isCurrency} className='-mb-[7.5vw] sm:mb-6' />
             </div>
         </div >
     );
